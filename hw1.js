b0b53eb3f113b3e229e41f7b6464501f4ca2c9c5
ymmousa <yasmin.mousa224@gmail.com>
2024-10-07 15:18:00 -0500

    Create hw1.js

---
+++ hw1.js
@@ -0,0 +1,20 @@
+/*
+    File: hw1.js
+    Name: Yasmin Mousa  
+    Date Created: 2024-09-04
+    Date Updated: 2024-09-21
+    Version: 1.0
+    Purpose: MIS 3371 Homework 1 JS File
+*/
+
+//this is the dynamic date js code//
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+
+//this is the range slider js code//
+let slider = document.getElementById("range")
+    let output = document.getElementById("range-slider")
+    output.innerHTML = slider.value;
+
+    slider.oninput = function () {output.innerHTML = this.value;};
